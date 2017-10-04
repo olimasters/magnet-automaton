2a97fd64915469d9397fc091af2f948610e4ea79
Oliver Masters <olimasters@hotmail.co.uk>
2017-10-04 23:00:07 +0100

    Tidied up Magnet and added a couple of new methods

--- src/Magnet.h
+++ src/Magnet.h
@@ -1,6 +1,7 @@
 #ifndef MAGNET_H
 #define MAGNET_H
 
+#include <random>
 #include <vector>
 
 class Magnet
@@ -13,12 +14,15 @@ class Magnet
         unsigned getWidth(void);
         const std::vector<std::vector<int>>& getSpins(void);
 
+        bool randomUpdate(void);
+            // Attemps a random update, returning true iff an update occurred.
         bool update(unsigned i, unsigned j);
             // Updates (i,j), if possible.  Returns true iff update occurred.
         bool isStable(void);
 
     private:
         std::vector<std::vector<int>> spins;
+        std::mt19937 rng;   // Mersenne twister random number generator.
         bool updatePending(unsigned i, unsigned j);
             // Whether or not (i,j) is due an update.
 };
